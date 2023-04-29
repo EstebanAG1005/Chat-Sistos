c04820964a3eb2b8311c1dc629f5851af03c6fd9
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-04-28 19:00:06 -0600

    Test

---
+++ client.c
@@ -0,0 +1,177 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "chat.pb-c.h" // Incluir el archivo generado por protoc para Protocol Buffers
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void *receive_handler(void *arg)
+{
+    int client_fd = *((int *)arg);
+    char buffer[BUFFER_SIZE];
+    int bytes_received;
+
+    while ((bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0)) > 0)
+    {
+        ChatSistOS__Answer *answer = chat_sist_os__answer__unpack(NULL, bytes_received, buffer);
+        if (answer == NULL)
+        {
+            perror("Error al decodificar el mensaje");
+            break;
+        }
+
+        switch (answer->op)
+        {
+        case 1: // Crear nuevo usuario
+        case 2: // Ver usuarios conectados
+        case 3: // Cambiar estado de usuario
+            printf("%s\n", answer->response_message);
+            break;
+        case 5: // Recibir mensaje
+            printf("[%s]: %s\n", answer->message->message_sender, answer->message->message_content);
+            break;
+        }
+
+        chat_sist_os__answer__free_unpacked(answer, NULL);
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    int client_fd;
+    struct sockaddr_in server_addr;
+    pthread_t thread_id;
+
+    client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_fd < 0)
+    {
+        perror("Error al crear el socket del cliente");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error en la conexión");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Cliente conectado al servidor.\n");
+
+    pthread_create(&thread_id, NULL, receive_handler, (void *)&client_fd);
+
+    char input[256];
+    ChatSistOS__UserOption user_option;
+    char buffer[BUFFER_SIZE];
+    int bytes_sent;
+    int op;
+
+    while (1)
+    {
+        printf("1. Crear nuevo usuario\n");
+        printf("2. Ver usuarios conectados\n");
+        printf("3. Cambiar estado de usuario\n");
+        printf("4. Enviar mensaje\n");
+        printf("5. Salir\n");
+        printf("Ingrese la opción que desea realizar: ");
+        scanf("%d", &op);
+
+        chat_sist_os__user_option__init(&user_option);
+
+        if (op == 1)
+        {
+            user_option.op = 1;
+            ChatSistOS__NewUser new_user = CHAT_SIST_OS__NEW_USER__INIT;
+            printf("Ingrese el nombre de usuario: ");
+            scanf("%s", input);
+            new_user.username = input;
+            user_option.createuser = &new_user;
+        }
+        else if (op == 2)
+        {
+            user_option.op = 2;
+            ChatSistOS__UserList user_list = CHAT_SIST_OS__USER_LIST__INIT;
+            printf("¿Desea ver la lista de todos los usuarios conectados? (1: sí, 0: no): ");
+            int list_all;
+            scanf("%d", &list_all);
+            user_list.list = list_all;
+
+            if (!list_all)
+            {
+                printf("Ingrese el nombre de usuario para obtener información: ");
+                scanf("%s", input);
+                user_list.user_name = input;
+            }
+
+            user_option.user_list = &user_list;
+        }
+                else if (op == 3)
+        {
+            user_option.op = 3;
+            ChatSistOS__Status status = CHAT_SIST_OS__STATUS__INIT;
+            printf("Ingrese el estado (1: en línea, 2: ocupado, 3: desconectado): ");
+            int new_status;
+            scanf("%d", &new_status);
+            status.user_state = new_status;
+            user_option.status = &status;
+        }
+        else if (op == 4)
+        {
+            user_option.op = 4;
+            ChatSistOS__Message message = CHAT_SIST_OS__MESSAGE__INIT;
+            printf("¿Desea enviar un mensaje privado? (1: sí, 0: no): ");
+            int private_message;
+            scanf("%d", &private_message);
+            message.message_private = private_message;
+
+            if (private_message)
+            {
+                printf("Ingrese el nombre de usuario del destinatario: ");
+                scanf("%s", input);
+                message.message_destination = input;
+            }
+
+            printf("Ingrese el contenido del mensaje: ");
+            scanf(" %[^\n]", input);
+            message.message_content = input;
+            user_option.message = &message;
+        }
+        else if (op == 5)
+        {
+            break;
+        }
+        else
+        {
+            printf("Opción no válida.\n");
+            continue;
+        }
+
+        int user_option_size = chat_sist_os__user_option__get_packed_size(&user_option);
+        chat_sist_os__user_option__pack(&user_option, buffer);
+
+        bytes_sent = send(client_fd, buffer, user_option_size, 0);
+        if (bytes_sent < 0)
+        {
+            perror("Error al enviar el mensaje al servidor");
+        }
+    }
+
+    close(client_fd);
+
+    return 0;
+}
+
+
+       
---
+++ server2.c
@@ -0,0 +1,221 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "chat.pb-c.h" // Incluir el archivo generado por protoc para Protocol Buffers
+
+#define PORT 8080
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+
+// Estructura para mantener la información del cliente
+typedef struct {
+    int client_fd;
+    struct sockaddr_in address;
+    pthread_t thread_id;
+    char username[32];
+    int user_state; // 1 en línea, 2 ocupado, 3 desconectado
+} client_t;
+
+client_t clients[MAX_CLIENTS];
+int num_clients = 0;
+
+void *client_handler(void *arg)
+{
+    client_t *client = (client_t *)arg;
+    char buffer[BUFFER_SIZE];
+    int bytes_received, bytes_sent;
+    int client_fd = client->client_fd;
+
+    ChatSistOS__Answer answer = CHAT_SIST_OS__ANSWER__INIT;
+    int answer_size;
+
+    // Recibimos mensajes del cliente
+    while ((bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0)) > 0)
+    {
+        // Procesamos el mensaje
+        ChatSistOS__UserOption *user_option = chat_sist_os__user_option__unpack(NULL, bytes_received, buffer);
+        if (user_option == NULL)
+        {
+            perror("Error al decodificar el mensaje");
+            break;
+        }
+
+        // Manejamos la opción
+        int operation = user_option->op;
+        if (operation == 1)
+        {
+            // Crear nuevo usuario
+            strcpy(client->username, user_option->createuser->username);
+
+            // Creamos el mensaje de respuesta de usuario
+            answer.response_message = "Usuario registrado exitosamente.";
+            answer_size = chat_sist_os__answer__get_packed_size(&answer);
+            chat_sist_os__answer__pack(&answer, buffer);
+
+            // Enviamos el mensaje al cliente
+            bytes_sent = send(client_fd, buffer, answer_size, 0);
+            if (bytes_sent < 0)
+            {
+                perror("Error al enviar el mensaje al cliente");
+            }
+
+            printf("Nuevo usuario creado: %s\n", client->username);
+        }
+        else if (operation == 2)
+        {
+            // Ver usuarios conectados
+            printf("Usuario %s solicitó lista de usuarios conectados\n", client->username);
+            printf("Lista de usuarios conectados:\n");
+            for (int i = 0; i < num_clients; i++)
+            {
+                if (clients[i].thread_id != 0)
+                {
+                    printf("- %s\n", clients[i].username);
+                }
+            }
+
+            // Creamos el mensaje de respuesta de usuario
+            answer.response_message = "Usuarios conectados mostrados.";
+            answer_size = chat_sist_os__answer__get_packed_size(&answer);
+            chat_sist_os__answer__pack(&answer, buffer);
+
+            // Enviamos el mensaje al cliente
+            bytes_sent = send(client_fd, buffer, answer_size, 0);
+            if (bytes_sent < 0)
+            {
+                perror("Error al enviar el mensaje al cliente");
+            }
+        }
+        else if (operation == 3)
+        {
+                        if (user_option->userlist != NULL && strlen(user_option->userlist->user_name) > 0)
+            {
+                for (int i = 0; i < num_clients; i++)
+                {
+                    if (strcmp(clients[i].username, user_option->userlist->user_name) == 0)
+                    {
+                        // Mostrar informacion
+                        char estado[20] = "";
+                        if (clients[i].user_state == 1)
+                        {
+                            strcpy(estado, "Disponible");
+                        }
+                        else if (clients[i].user_state == 2)
+                        {
+                            strcpy(estado, "Ocupado");
+                        }
+                        else if (clients[i].user_state == 3)
+                        {
+                            strcpy(estado, "Ausente");
+                        }
+
+                        printf("Información del usuario solicitado:\n");
+                        printf("Nombre de usuario: %s\n", clients[i].username);
+                        printf("Estado: %s\n", estado);
+
+                        // Creamos el mensaje de respuesta de usuario
+                        answer.response_message = "Información del usuario mostrada.";
+                        answer_size = chat_sist_os__answer__get_packed_size(&answer);
+                        chat_sist_os__answer__pack(&answer, buffer);
+
+                        // Enviamos el mensaje al cliente
+                        bytes_sent = send(client_fd, buffer, answer_size, 0);
+                        if (bytes_sent < 0)
+                        {
+                            perror("Error al enviar el mensaje al cliente");
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                // Cambiar estado del usuario
+                client->user_state = user_option->status->user_state;
+
+                // Creamos el mensaje de respuesta de usuario
+                answer.response_message = "Estado cambiado.";
+                answer_size = chat_sist_os__answer__get_packed_size(&answer);
+                chat_sist_os__answer__pack(&answer, buffer);
+
+                // Enviamos el mensaje al cliente
+                bytes_sent = send(client_fd, buffer, answer_size, 0);
+                if (bytes_sent < 0)
+                {
+                    perror("Error al enviar el mensaje al cliente");
+                }
+            }
+        }
+
+        chat_sist_os__user_option__free_unpacked(user_option, NULL);
+    }
+
+    printf("Cliente desconectado: %s\n", client->username);
+    close(client_fd);
+    client->thread_id = 0;
+
+    return NULL;
+}
+
+int main()
+{
+    int server_fd, new_client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+    pthread_t thread_id;
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0)
+    {
+        perror("Error al crear el socket del servidor");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error en el bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("Error en el listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Servidor iniciado. Esperando clientes...\n");
+
+    while ((new_client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_size)))
+    {
+        printf("Nuevo cliente conectado\n");
+
+        if (num_clients >= MAX_CLIENTS)
+        {
+                        printf("Se alcanzó el máximo número de clientes.\n");
+            close(new_client_fd);
+            continue;
+        }
+
+        client_t *client = &clients[num_clients++];
+        client->client_fd = new_client_fd;
+
+        pthread_create(&thread_id, NULL, client_handler, (void *)client);
+        client->thread_id = thread_id;
+    }
+
+    close(server_fd);
+
+    return 0;
+}
+
+
